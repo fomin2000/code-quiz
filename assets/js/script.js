d3e2dac37fe9ddf948ad54fd8fcd83513c17b626
Pasha Fomin <pashafomin@Pashas-MacBook-Pro.local>
2022-10-15 14:45:28 -0700

    finished all javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,45 @@
 // Globals
 var startButton = document.querySelector('.startGame')
 var timeAmount = document.querySelector('.timeLeft')
-var questionContainer = document.querySelector('.questionContainer')
-var questionTitle = document.createElement('h1')
-var listEl = document.createElement('ul')
-var li1 = document.createElement('li')
-var li2 = document.createElement('li')
-var li3 = document.createElement('li')
-var li4 = document.createElement('li')
 
-var listStyles = [li1, li2, li3, li4]
 
-for (let i = 0; i < listStyles.length; i++) {
-    listStyles[i].setAttribute("style", "width: 33%; height:")
-    
-}
+var mainInfo = document.querySelector('.mainInfo')
+var titleContainer = document.querySelector('.titleContainer')
+var containerTitle = document.querySelector('.containerTitle')
+var titleIntro = document.querySelector('titleIntro')
+
+
+var choice1 = document.querySelector('.choice1')
+var choice2 = document.querySelector('.choice2')
+var choice3 = document.querySelector('.choice3')
+var choice4 = document.querySelector('.choice4')
+
+
+
+var hiddenElement = document.querySelector('.hiddenDiv')
+var questionTitle = document.querySelector('.questionTitle')
+var choicesList = document.querySelector('.choices')
+var amountCorrect = document.querySelector('.amountCorrect')
+
+var hiddenCorrect = document.querySelector('.hidden3')
+var hiddenWrong = document.querySelector('.hidden4')
+
+var endMessage = document.querySelector('.hidden2')
+
+var currentAnswer;
+
+var correct = 0
+
+var questionsAnswered = 0
+
+
+
+// for (let i = 0; i < listStyles.length; i++) {
+    // listStyles[i].setAttribute("style", "width: 33%; height: 50px; background-color: rebeccapurple; border: 1px solid rebeccapurple; border-raidus: 5px;")}
 
 var initialTime = 60
 var timeLeft = null
+var isCorrect = null
 
 /**
  * get elements
@@ -44,35 +66,135 @@ var timeLeft = null
  * - remove high score data
  * 
  */
-var questions = ['People that write code are called: ', 'Which document mostly answers for styling?', 'JavaScript is used for: ', 'HTML is an acronym for: ', 'In html, an <ol> is reffered to: ']
+// var questions = ['People that write code are called: ', 'Which document mostly answers for styling?', 'JavaScript is used for: ', 'HTML is an acronym for: ', 'In html, an <ol> is reffered to: ']
+
+var questions = [
+    {
+        question: 'People that write code are called: ',
+        answers: ['Manufacturers', 'Cryptographers', 'Programmers', 'Professors'],
+        correctAnswer: 'Programmers',
+    },
+    {
+        question: 'Which document mostly answers for styling?',
+        answers: ['CSS', 'HTML', 'JavaScript', 'C++'],
+        correctAnswer: 'CSS',
+    },
+    {
+        question: 'JavaScript is used for: ',
+        answers: ['skeleton page markup', 'styling', 'functionality', 'Can be used for all of the above'],
+        correctAnswer: 'Can be used for all of the above',
+    },
+    {
+        question: 'HTML is an acronym for: ',
+        answers: ['Hello There Mr Larry', 'HyperText Markup Language', 'Header Title Main Link', 'None of the above'],
+        correctAnswer: 'HyperText Markup Language',
+    },
+    {
+        question: 'In html, an <ol> is reffered to: ',
+        answers: ['Ordered List', 'Unordered List', 'List Item', 'Table Row'],
+        correctAnswer: 'Ordered List',
+    },
+    
+]
+
 
 timeAmount.textContent = 0
+
+function hideMain() {
+    mainInfo.style.display = 'none'
+}
+
+function showQuestion () {
+    hiddenElement.style.display = 'block'
+}
 
 function startTimer() {
     timeLeft = initialTime
     var timerInterval = setInterval(function () {
-        if (timeLeft > 0) {
-            timeLeft--;
-            timeAmount.textContent = timeLeft
-        } else {
-            gameOver()
+        timeLeft--
+        timeAmount.textContent = timeLeft
+        if(timeLeft <= 0) {
+            clearInterval(timerInterval)
+            timeAmount.textContent = 0
+            displayMessage()
+        } else if (questionsAnswered > 4){
+            clearInterval(timerInterval)
+            timeAmount.textContent = 0
+            displayMessage()
+            hiddenElement.style.display = 'none'
         }
     }, 1000)
 }
 
-function startGame() {
+function endGame() {
+    timeLeft = 0
+    displayMessage()
+}
+
+function displayMessage() {
+    endMessage.style.display = 'block'
+    amountCorrect.textContent = `Number of answers correct: ${correct}`
+}
+
+function startGame(e) {
+    e.preventDefault()
+    hideMain()
+    showQuestion ()
     startTimer()
-    questionContainer.textContent = ''
+
+
+    var question1 = getQuestion()
+    newQuestion(question1)
+    
 }
 
-function gameOver() {
-    clearInterval(timerInterval)
+
+function newQuestion(questionObj) {
+    getQuestion() 
+
+    var title = questionObj.question
+    var choices = questionObj.answers
+    var answer = questionObj.correctAnswer
+    currentAnswer = answer
+
+    questionTitle.textContent = title
+    choice1.textContent = choices[0]
+    choice2.textContent = choices[1]
+    choice3.textContent = choices[2]
+    choice4.textContent = choices[3]
+
+    hiddenCorrect.style.display = 'none'
+    hiddenWrong.style.display = 'none'
 }
 
-function newQuestion() {
-
+function getQuestion() {
+    var randomNumber = Math.floor(Math.random() * questions.length)
+    return questions[randomNumber]
 }
+
+
 
 
 //  Listeners
 startButton.addEventListener('click', startGame)
+
+choicesList.addEventListener('click', function(e) {
+    var usersPick = e.target.textContent
+
+    if (usersPick === currentAnswer) {
+        correct++
+        questionsAnswered++
+        var nextQuestion = getQuestion()
+        newQuestion(nextQuestion)
+        hiddenCorrect.style.display = 'block'
+    } else {
+        questionsAnswered++
+        timeLeft = timeLeft - 10
+        var nextQuestion = getQuestion()
+        newQuestion(nextQuestion)
+        hiddenWrong.style.display = 'block'
+    }
+    console.log(questionsAnswered)
+})
+
+console.log(questionsAnswered)
